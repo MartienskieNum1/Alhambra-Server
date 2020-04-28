e2d3156d9556b02f5bdddb51fdf1c04f52bce4f6
sam.michiels <sam.michiels@student.howest.be>
2020-04-28 14:52:57 +0200

    velden + constructor klaar

--- src/main/java/be/howest/ti/alhambra/logic/Wall.java
+++ src/main/java/be/howest/ti/alhambra/logic/Wall.java
@@ -1,4 +1,23 @@
 package be.howest.ti.alhambra.logic;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
 public class Wall {
+    private final boolean north;
+    private final boolean east;
+    private final boolean south;
+    private final boolean west;
+
+    @JsonCreator
+    public Wall(@JsonProperty("north")boolean north,@JsonProperty("east") boolean east,@JsonProperty("south")boolean south,@JsonProperty("west") boolean west) {
+        this.north = north;
+        this.east = east;
+        this.south = south;
+        this.west = west;
+    }
+
+
 }
