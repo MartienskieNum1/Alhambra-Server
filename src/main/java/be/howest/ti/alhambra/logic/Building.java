10d9c8cff0aed14400322948dcf5616c92d439eb
sam.michiels <sam.michiels@student.howest.be>
2020-04-28 12:01:39 +0200

    jsoncreator in constructor, equals en hashcode en tostring gemaakt

--- src/main/java/be/howest/ti/alhambra/logic/Building.java
+++ src/main/java/be/howest/ti/alhambra/logic/Building.java
@@ -1,20 +1,45 @@
 package be.howest.ti.alhambra.logic;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Building {
     private final BuildingType buildingType;
     private final int cost;
     private final Map<Wall, Boolean> walls = new HashMap<>();
 
-    public Building(BuildingType buildingType, int cost) {
+
+    @JsonCreator
+    public Building(@JsonProperty("buildingType") BuildingType buildingType,@JsonProperty("cost") int cost) {
         this.buildingType = buildingType;
         this.cost = cost;
     }
 
-    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Building building = (Building) o;
+        return cost == building.cost &&
+                buildingType == building.buildingType &&
+                Objects.equals(walls, building.walls);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(buildingType, cost, walls);
+    }
 
-
+    @Override
+    public String toString() {
+        return "Building{" +
+                "buildingType=" + buildingType +
+                ", cost=" + cost +
+                ", walls=" + walls +
+                '}';
+    }
 }
