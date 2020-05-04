fe61e86efb12a81e3e605ec682201f83a2777447
niels.baudewijns <niels.baudewijns@student.howest.be>
2020-05-04 14:35:01 +0200

    GameId is 3 digits

--- src/main/java/be/howest/ti/alhambra/logic/Alhambra.java
+++ src/main/java/be/howest/ti/alhambra/logic/Alhambra.java
@@ -1,17 +1,29 @@
 package be.howest.ti.alhambra.logic;
 
+import org.apache.commons.lang3.builder.StandardToStringStyle;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class Alhambra {
     public static List<Game> games = new LinkedList<>();
     private static final String groupNr = "group27";
+    private static int gameIdCounter = 0;
+    private static String gameIdString = "";
     private static int gameId = 0;
 
     public static Game addGame(){
+        gameId = gameIdCounter;
+        if (gameIdCounter < 10){
+            gameIdString = "00" + gameIdCounter;
+        }
+        else if (gameIdCounter < 100){
+            gameIdString = "0" + gameIdCounter;
+        }
+        gameId = Integer.parseInt(gameIdString);
         Game newlyMadeGame = new Game(groupNr, gameId, new LinkedList<>());
            games.add(newlyMadeGame);
-           gameId += 1;
+           gameIdCounter += 1;
            return newlyMadeGame;
     }
 
