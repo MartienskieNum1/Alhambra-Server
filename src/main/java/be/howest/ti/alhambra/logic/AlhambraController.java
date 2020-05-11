0bb884e023e94136972b8a8b7b0fd7fc4b2b9992
sam.michiels <sam.michiels@student.howest.be>
2020-05-11 17:40:14 +0200

    code opkuisen

--- src/main/java/be/howest/ti/alhambra/logic/AlhambraController.java
+++ src/main/java/be/howest/ti/alhambra/logic/AlhambraController.java
@@ -1,12 +1,9 @@
 package be.howest.ti.alhambra.logic;
 
 import io.vertx.core.json.Json;
-
-import java.util.LinkedList;
 import java.util.List;
 
 public class AlhambraController {
-    private List<Building> buildings = new LinkedList<>();
 
     public Currency[] getCurrencies() {
         return Currency.values();
@@ -17,7 +14,6 @@ public class AlhambraController {
     }
 
     public List<Building> getBuildings() {
-
         return BuildingFactory.getAllBuildings();
     }
 
