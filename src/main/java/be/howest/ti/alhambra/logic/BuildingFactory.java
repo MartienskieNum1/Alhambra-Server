951461bda337832d2de101ad79cbec7a3f8569c8
Martienskie <maarten.de.meyere@student.howest.be>
2020-05-12 20:34:10 +0200

    modify returned list

--- src/main/java/be/howest/ti/alhambra/logic/BuildingFactory.java
+++ src/main/java/be/howest/ti/alhambra/logic/BuildingFactory.java
@@ -5,6 +5,7 @@ import io.vertx.core.json.Json;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -27,8 +28,8 @@ public class BuildingFactory {
 
     private static List<Building> loadFromFile() {
         try (InputStream input = BuildingFactory.class.getResourceAsStream("/buildings.json")) {
-            return Arrays.asList(Json.decodeValue(Buffer.buffer(input.readAllBytes()),
-                            Building[].class)
+            return new ArrayList<>(Arrays.asList(Json.decodeValue(Buffer.buffer(input.readAllBytes()),
+                            Building[].class))
             );
         } catch (IOException ex) {
             Logger.getAnonymousLogger().log(Level.SEVERE, "Didn't find the buildings");
