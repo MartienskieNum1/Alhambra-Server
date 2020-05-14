8020ab870d62a5370011bb7adabb2a972b844f30
aron.delbol <aron.delbol@student.howest.be>
2020-05-14 15:56:12 +0200

    fix tests

--- src/test/java/be/howest/ti/alhambra/logic/AlhambraTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/AlhambraTest.java
@@ -10,7 +10,7 @@ class AlhambraTest {
     void clearGames() {
         Alhambra alhambra = new Alhambra();
         for (int i=0; i<5; i++){
-            alhambra.addGame();
+            alhambra.addGame("group27");
         }
         alhambra.clearGames();
 
@@ -21,7 +21,7 @@ class AlhambraTest {
     void addGame() {
         Alhambra alhambra = new Alhambra();
         for (int i=0; i<5; i++){
-            alhambra.addGame();
+            alhambra.addGame("group27");
         }
 
         assertEquals(5, alhambra.games.size());
