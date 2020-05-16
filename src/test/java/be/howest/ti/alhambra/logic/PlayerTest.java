4276e7922bab234b0042562018aa402b8fa28b13
Martienskie <maarten.de.meyere@student.howest.be>
2020-05-16 18:43:45 +0200

    placeholder for test

--- src/test/java/be/howest/ti/alhambra/logic/PlayerTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/PlayerTest.java
@@ -1,16 +1,35 @@
 package be.howest.ti.alhambra.logic;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    Alhambra alhambra = new Alhambra();
+    Game myGame;
+    Player player1 = new Player("maarten");
+    Player player2 = new Player("jos");
+    Building building1 = new Building(BuildingType.GARDEN, 9);
+
+    @BeforeEach
+    void fillAlhambra() {
+        alhambra.addGame("group27"); //group27-000
+        myGame = alhambra.findGame("group27-000");
+
+        myGame.addPlayer("group27-000+maarten", player1);
+        myGame.addPlayer("group27-000+jos", player2);
+        myGame.setPlayerReady("group27-000+maarten");
+        myGame.setPlayerReady("group27-000+jos");
+    }
 
     @Test
     void buildBuilding() {
+        //
     }
 
     @Test
     void placeInReserve() {
+        //
     }
 }
