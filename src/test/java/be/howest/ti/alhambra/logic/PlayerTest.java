b34101a712f1f2fa3e0d660a1bb72fd77f7093e5
Martienskie <maarten.de.meyere@student.howest.be>
2020-05-16 16:21:30 +0200

    init PlayerTest

---
+++ src/test/java/be/howest/ti/alhambra/logic/PlayerTest.java
@@ -0,0 +1,16 @@
+package be.howest.ti.alhambra.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void buildBuilding() {
+    }
+
+    @Test
+    void placeInReserve() {
+    }
+}
