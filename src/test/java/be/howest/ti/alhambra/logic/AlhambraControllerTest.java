1c708640b4978104f9d3b1d2df9c6bb67057c350
Martienskie <maarten.de.meyere@student.howest.be>
2020-05-23 20:03:57 +0200

    change test for new player token

--- src/test/java/be/howest/ti/alhambra/logic/AlhambraControllerTest.java
+++ src/test/java/be/howest/ti/alhambra/logic/AlhambraControllerTest.java
@@ -9,13 +9,14 @@ class AlhambraControllerTest {
     AlhambraController controller = new AlhambraController();
 
     @Test
-    void returnPlayerToken() {
+    void getPlayerToken() {
         alhambra.addGame("group27"); //group27-000
         Game myGame = alhambra.findGame("group27-000");
 
         Player player1 = new Player("maarten");
 
-        assertEquals("group27-000+maarten", controller.getPlayerToken(myGame, player1));
+        assertEquals("147/162/157/165/160/62/67/55/60/60/60/53/155/141/141/162/164/145/156/",
+                controller.getPlayerToken(myGame, player1));
         assertThrows(IllegalArgumentException.class, () -> controller.getPlayerToken(null, player1));
 
     }
